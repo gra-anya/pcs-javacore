9cf91897ca5570705d3e007b4377a1b180b0116f
Анна Евграфова <gra-anya@mail.ru>
2022-03-24 10:42:31 +0300

    Add Todos, TodoServer

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,6 +1,6 @@
 package ru.netology.javacore;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -8,14 +8,41 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class TodoServer {
-    //...
+    private int port;
+    private Todos todos;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.port = port;
+        this.todos = todos;
     }
 
-    public void start() throws IOException {
+    public void start() {
         System.out.println("Starting server at " + port + "...");
-        //...
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket clientSocket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+            while (true) {
+                String taskJson = in.readLine();
+                JsonElement rootNode = JsonParser.parseString(taskJson);
+                JsonObject details = rootNode.getAsJsonObject();
+
+                String type = details.get("type").getAsString();
+                String task = details.get("task").getAsString();
+
+                switch (type) {
+                    case "ADD":
+                        todos.addTask(task);
+                        break;
+                    case ("REMOVE"):
+                        todos.removeTask(task);
+                        break;
+                }
+                out.println(todos.getAllTasks());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -4,18 +4,24 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Todos {
-    //...
+
+    private List<String> tasks = new ArrayList<>();
 
     public void addTask(String task) {
-        //...
+        tasks.add(task);
     }
 
     public void removeTask(String task) {
-        //...
+        tasks.remove(task);
     }
 
     public String getAllTasks() {
-        //...
+        Collections.sort(tasks);
+        StringBuilder builder = new StringBuilder();
+        for (String task : tasks) {
+            builder.append(task).append(" ");
+        }
+        return builder.toString();
     }
 
 }
